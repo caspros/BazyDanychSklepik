396a9db55bcdff73ba48ccfc42c3621feb1297c3
caspros <56698760+caspros@users.noreply.github.com>
2019-10-18 11:58:17 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Alledrogo</title>
+</head>
+
+<body>
+	
+<?php
+	echo Witamy
+?>
+
+</body>
+</html>
