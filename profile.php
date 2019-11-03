5c4db498032b5397a8b4d66dbbb6468395be67a8
armanii64 <57318387+armanii64@users.noreply.github.com>
2019-11-03 15:27:40 +0100

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,6 +2,7 @@
 echo "To jest profil uzytkownika <br>";
 echo "A TUTAJ NAGIE FOTKI <br>";
 echo "Serio myślałem, że to te z wakacji...."
+echo "Panowie bez fotek kutasów bo zawiadomie Inżyniera Dudka"
 echo '<a href="index.php">STRONA GŁÓWNA</a>'
 echo '<br>test1'
 ?>
