000bf4eb8d8334f0bc8c6c5fb8520300b96931f2
Kacper Sledzik <caspros97@gmail.com>
2019-12-13 16:14:02 +0100

    Changed dropdown menu in each file

--- koszyk.php
+++ koszyk.php
@@ -132,8 +132,9 @@
 
 				<!-- DROPDOWN CONTENT -->
 	  			<div id="myDropdown" class="dropdown-content">
-		    		<a href="zamowienia.php">Moje zamówienia</a>
-					<a href="#">Oceń produkt</a>
+		    		<a href="zamowienia.php">Zamówienia</a>
+					<a href="ocena_produktu.php">Oceń produkt</a>
+					<a href="ocena_sklepu.php">Oceń sklep</a>
 					<a href="profil.php">Ustawienia</a>
 					<?php
 						if (isset($_SESSION['zaloguj']))
