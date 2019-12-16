e84dd78ef8279b80b3556b0ec4a0f67fb4f7c374
Mono97 <57317973+Mono97@users.noreply.github.com>
2019-12-16 19:32:26 +0100

    Regulamin w dwóch językach

---
+++ regulamineng.php
@@ -0,0 +1,152 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+
+<head>
+	<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/koszyk.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/menu.css">
+	<link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap&subset=latin-ext" rel="stylesheet">
+	<link href="fontawesome/css/all.css" rel="stylesheet">
+	<title>Ocena sklepu</title>
+</head>
+<style>
+	.error
+		{
+			color:red;
+			margin-top: 10px;
+			margin-bottom: 10px;
+		}
+</style>
+
+<body>
+	<!-- STICKY MENU -->
+	<div id="sticky_menu">
+		<ol>
+			<!-- logo alledrogo -->
+			<li>
+				<a href="index.php">
+					<img src="images/logo.png" alt="logo" class="nav_img">
+				</a>
+			</li>
+
+			<!-- wyszukiwanie -->
+			<form action="wyszukaj.php" method="get" class="form_inline">
+				<li>
+					<input type="text" name="search_input" class="search_input" placeholder="Wyszukaj produkt...">
+				</li>
+
+				<li>
+					<input style="display: inline;" type="submit" name="search_button" class="search_button" value="SZUKAJ">
+				</li>
+			</form>
+
+			<!-- koszyk -->
+			<li>
+				<a href="koszyk.php">
+					<span class="koszyk">
+						<i class="fas fa-shopping-cart"></i>
+					</span>
+				</a>
+			</li>
+
+			<!-- DROPDOWN BUTTON -->
+			<div class="dropdown">
+				<span id="myBtn" class="dropbtn">Witaj, 
+					<?php 
+					if (isset($_SESSION['wyloguj']))
+						{
+							echo $_SESSION['imie'];
+						} else {
+							echo "zaloguj się";
+						}
+					?> 
+
+					<div id="p1">
+						<i class="fas fa-angle-down"></i>
+					</div>
+				</span>
+
+				<!-- DROPDOWN CONTENT -->
+	  			<div id="myDropdown" class="dropdown-content">
+		    		<a href="zamowienia.php">Zamówienia</a>
+					<a href="ocena_produktu.php">Oceń produkt</a>
+					<a href="ocena_sklepu.php">Oceń sklep</a>
+					<a href="profil.php">Ustawienia</a>
+					<?php
+						if (isset($_SESSION['zaloguj']))
+						{
+							echo '<a href="logowanie.php">'.$_SESSION['zaloguj'].'</a>';
+						} else{
+							echo '<a href="wyloguj.php">'.$_SESSION['wyloguj'].'</a>';
+						}
+					?>
+	  			</div>
+			</div>
+		</ol>
+	</div>
+	
+	<!-- GŁÓWNY CONTAINER -->
+	<div id="container_koszyk">
+		
+		<h1>Alledrogo User Agreement:</h1>
+		
+		<div id="koszyk_container">
+				<div id="informacja1">
+				<br><br>
+					<h3>PRIVACY AND CONFIDENTIALITY</h3><br>
+
+<p>1.Personal data provided by Users in designated areas shall be processed by Alledrogo pursuant to the applicable provisions of law and the privacy policy.</p><br>
+
+<p>2.Users’ personal data may be disclosed to other Users only in the cases set forth herein for purposes related to the Transactions and in other cases with prior consent of the data subject.</p><br>
+
+<p>3.A User shall not disclose to third parties any information related to other Users received from Alledrogo in connection with the use of Alledrogo, unless the User has obtained prior consent of the User who is the data subject. In particular, the use of such information for commercial purposes, in particular to promote in any form the activities of any User carried out outside Alledrogo shall be prohibited.</p><br>
+
+<p>4.The use of the information referred to in Article 3, for commercial purposes to promote in any form the activities of any User carried out outside Alledrogo shall be prohibited. In particular the following shall be prohibited:</p>
+
+<p>a) making proposals to buy or sell the Item outside Alledrogo;</p>
+
+<p>b) adding to the shipped Item or providing in any other form any content with information about commercial activities carried out outside Alledrogo (e.g. leaflets advertising an online store);</p>
+
+<p>c) opening accounts for Users in online stores.</p><br>
+
+<p>5.Alledrogo reserves the right to filter and block messages sent by Users using the tools made available on Alledrogo, in particular if they are spam, include content that violates this User Agreement or otherwise pose a risk to the safety of Users.</p><br>
+
+<p>6.Ended Listings may be published in archiwum.alledrogo.pl subdomain. Published information is for reference only and Alledrogo may not ensure or guarantee that ended Listings will be made public in a full and comprehensive manner.</p><br>
+
+
+				</div>
+			<br><br>
+			
+		</div>
+	</div>
+
+    <div id="centeredmenu">
+	   <ul>
+	      <li><a href="FAQ.php">FAQ</a></li>
+	      <li><a href="kontakt.php">Kontakt</a></li>
+	      <li><a href="regulamin.php">Regulamin</a></li>
+	   </ul>
+	</div>
+
+	<div id="footer">
+		Korzystanie z serwisu oznacza akceptację
+		<a href="regulamin.php">
+			regulaminu
+		</a>
+	</div>	
+
+	<!-- JQUERY -->
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+	<!-- STICKY MENU JS-->
+	<script src="js/sticky_menu.js"></script>
+	<!-- STICKY MENU WITAJ ZALOGUJ SIĘ JS-->
+	<script src="js/dropdown_sticky.js"></script>
+	<!-- SLIDER JS-->
+	<script src="js/slider.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	
+</body>
+</html>
