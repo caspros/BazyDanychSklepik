18bc49d56021189dbb1a181473e515307cfe754d
Kacper Sledzik <caspros97@gmail.com>
2020-01-15 12:35:22 +0100

    Poprawione odnosniki wyroznionych produktow w index.php

--- index.php
+++ index.php
@@ -110,12 +110,12 @@
 			<div id="products">
 				<br>
 
-				<a href="produkt.php?id_produkty=12"><div class="product"><?php Show_product(53);?></div></a>
-				<a href="produkt.php?id_produkty=14"><div class="product"><?php Show_product(52);?></div></a>
-				<a href="produkt.php?id_produkty=8"><div class="product"><?php Show_product(84);?></div></a>
-				<a href="produkt.php?id_produkty=15"><div class="product"><?php Show_product(93);?></div></a>
-				<a href="produkt.php?id_produkty=16"><div class="product"><?php Show_product(56);?></div></a>
-				<a href="produkt.php?id_produkty=17"><div class="product"><?php Show_product(94);?></div></a>
+				<a href="produkt.php?id_produkty=53"><div class="product"><?php Show_product(53);?></div></a>
+				<a href="produkt.php?id_produkty=52"><div class="product"><?php Show_product(52);?></div></a>
+				<a href="produkt.php?id_produkty=84"><div class="product"><?php Show_product(84);?></div></a>
+				<a href="produkt.php?id_produkty=93"><div class="product"><?php Show_product(93);?></div></a>
+				<a href="produkt.php?id_produkty=56"><div class="product"><?php Show_product(56);?></div></a>
+				<a href="produkt.php?id_produkty=94"><div class="product"><?php Show_product(94);?></div></a>
 
 			</div>
 
